009b5497c7b6a4b8d6f9920f5955426303b81fa5
Mauricio Arce <Mauricio.Arce@communitybrands.com>
2019-01-23 18:27:10 -0400

    Spaces for environment variables were added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
+  },
+  googlemaps: '',
+  mapbox: {
+    accessToken: ''
+  }
 };
