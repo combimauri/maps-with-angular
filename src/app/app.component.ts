db757979f5af726e07c777c74474796dd1520d1e
Mauricio Arce <Mauricio.Arce@communitybrands.com>
2019-01-23 10:29:14 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-maps-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'maps-with-angular';
+}
